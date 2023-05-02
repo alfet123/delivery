b68f78551058a488d3d53411a1e134fed5e0904d
Alexey Fetisov <alfet123@gmail.com>
2023-05-02 15:16:30 +0500

    Работа с датами

--- tools/date.php
+++ tools/date.php
@@ -4,30 +4,32 @@ date_default_timezone_set("Asia/Yekaterinburg");
 
 if (isset($argv[1])) {
 
-$date_message = "Указанная дата";
-$week_message = "Находится на неделе";
+    $date_message = "Указанная дата";
+    $week_message = "Находится на неделе";
 
-$date = new DateTimeImmutable($argv[1]);
+    $time = strtotime($argv[1]);
 
 } else {
 
-$date_message = "Текущая дата";
-$week_message = "Текущая неделя";
+    $date_message = "Текущая дата";
+    $week_message = "Текущая неделя";
 
-$date = new DateTimeImmutable();
+    $time = time();
 
 }
 
-echo $date_message.": ".date("d.m.Y", $date).PHP_EOL;
+$date = date("d.m.Y", $time);
 
-$current_week_day = $date->format("w");
+$current_week_day = date_format(date_create($date), "w");
 
 $days_to_week_end = 7 - $current_week_day;
 
 $days_to_week_start = 6 - $days_to_week_end;
 
-$first_week_day_date = mktime(0, 0, 0, date("m", $date), date("d", $date) - $days_to_week_start, date("Y", $date));
+$first_week_day = mktime(0, 0, 0, date("m", $time), date("d", $time) - $days_to_week_start, date("Y", $time));
 
-$last_week_day_date = mktime(0, 0, 0, date("m", $date), date("d", $date) + $days_to_week_end, date("Y", $date));
+$last_week_day = mktime(0, 0, 0, date("m", $time), date("d", $time) + $days_to_week_end, date("Y", $time));
 
-echo $week_message.": ".date("d.m.Y", $first_week_day_date)." - ".date("d.m.Y", $last_week_day_date).PHP_EOL;
+echo $date_message.": ".$date.PHP_EOL;
+
+echo $week_message.": ".date("d.m.Y", $first_week_day)." - ".date("d.m.Y", $last_week_day).PHP_EOL;
